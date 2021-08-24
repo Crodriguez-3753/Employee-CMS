a8aa4e221f6bb73f51b0c4d2fbbe99d2d1989289
Carlos Rodriguez <crodriguez3753@gmail.com>
2021-08-24 13:42:03 -0400

    initial files

---
+++ index.js
@@ -0,0 +1,39 @@
+const questions = require("./utils/questions");
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+
+async function main(){
+    
+    console.log(questions.internQuestions)
+    // htmlCardArray = [];
+
+    // generate manager
+        // ask questions 
+        // render new manger based on object props 
+        // push to htmlCardArray
+        // to mainMeu
+    
+    // generate Engineer
+        // ask questions 
+        // render new manger based on object props 
+        // push to htmlCardArray
+    
+    // generate Intern
+        // ask questions 
+        // render new manger based on object props 
+        // push to htmlCardArray
+        // back to mainMenu
+
+    // mainMenu func
+        // generate Engineer
+        // generate Intern
+        // bye
+
+
+
+
+
+}
+
+// Start 🚀
+main();
